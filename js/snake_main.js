bcab3202f4df73f996f9a8e9ee44590621e4b559
sleeppleasebutcant <widelec0700.880@gmail.com>
2020-07-07 22:41:32 +0200

    snake now can turn

--- js/snake_main.js
+++ js/snake_main.js
@@ -4,7 +4,7 @@ import { Snake, DIRECTION } from "./snake_logic.js";
 var KEYS = []
 var ARROW_KEY = { up: 87, down: 83, right: 68, left: 65 };
 
-var snake = new Snake(30);
+var snake = new Snake(1000);
 var offset = 0;
 
 
@@ -14,50 +14,46 @@ const context = canvas.getContext("2d");
 
 
 
-var pixel = context.createI
-mageData(1, 1);
+var pixel = context.createImageData(1, 1);
 pixel.data[0] = 0xff; pixel.data[1] = 0; pixel.data[2] = 0; pixel.data[3] = 0xff;
 
 function draw() {
     console.log('clearing...');
     context.clearRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < snake.body.length; i++) {
-        console.log(snake.head);
-        console.log(pixel, snake.body[i].x + offset, snake.body[i].y + offset);
+        console.log(snake.head.x);
+        //console.log(pixel, snake.body[i].x + offset, snake.body[i].y + offset);
         context.putImageData(pixel, snake.head.x + offset, snake.head.y + offset);
         context.putImageData(pixel, snake.body[i].x + offset, snake.body[i].y + offset);
     }
 }
 
-function handleUserInput() {
-    if (KEYS[ARROW_KEY.up]) {
-        snake.turnUp();
-        KEYS[ARROW_KEY.up]=false;
-
-    } else if (KEYS[ARROW_KEY.down]) {
-        snake.turnDown();
-        KEYS[ARROW_KEY.down]=false;
-
-    }
-    else if (KEYS[ARROW_KEY.right]) {
-        snake.turnRight();
-        KEYS[ARROW_KEY.right]=false;
-
-    }
-    else if (KEYS[ARROW_KEY.left]) {
-        snake.turnLeft();
-        KEYS[ARROW_KEY.left]=false;
-
-    }
-
-}
 
 
 function gameLoop() {
-    handleUserInput();
+    console.log("tick");
+    console.log(snake);
+    snake.tick();   
     draw();
-    snake.tick();
 }
+
+$(document).on("keydown", function (e) {
+    console.log("keycode:" + e.which);
+    console.log(String.fromCharCode(e.which));
+    if(String.fromCharCode(e.which) == 'W')
+    {
+        snake.turnUp();
+    } else if (String.fromCharCode(e.which) == 'S') {
+        snake.turnDown();
+    } else if (String.fromCharCode(e.which) == 'D') {
+        snake.turnRight();
+    } else if(String.fromCharCode(e.which) == 'A')
+    {
+        snake.turnLeft();
+    }
+});
+
+
 
 setInterval(gameLoop, 100);
 document.addEventListener("keydown", e => KEYS[e.keyCode] = true, false);
