9c2fbc252921f14a1fb82007642e70b1b7237891
sleeppleasebutcant <widelec0700.880@gmail.com>
2020-07-10 01:15:49 +0200

    added function to generate random rgb string

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,17 @@
+
+
 export default function range(n)
 {
     return new Array(n).fill(0);
+}
+
+export function randomColor()
+{
+    let alph='0123456789abcdef';
+    let r = "#";
+    for(let i in range(6))
+    {
+        r+=`${alph[Math.round((Math.random()*100))%16]}`;
+    }
+    return r;
 }
